b42246a7385d51904bc772fb538d4a0ef62e4444
Stefan <git@stefn.co.uk>
2025-08-08 20:09:07 +0100

    Remove stub factories

--- spec/factories/units.rb
+++
@@ -1,9 +0,0 @@
-# typed: false
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :unit do
-    id { SecureRandom.alphanumeric(12) }
-    user_id { SecureRandom.alphanumeric(12) }
-  end
-end
--- spec/factories/users.rb
+++
@@ -1,8 +0,0 @@
-# typed: false
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :user do
-    id { SecureRandom.alphanumeric(12) }
-  end
-end
