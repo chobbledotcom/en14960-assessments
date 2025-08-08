d03f9d3dd11a2caec23b32100f82e7e1960d90b7
Stefan <git@stefn.co.uk>
2025-08-08 14:35:35 +0100

    Move factories here

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,9 @@ require "rails"
 require "active_record"
 require "en14960_assessments"
 
+# Set the root to the gem directory to avoid loading parent app config
+Rails.application.config.root = Pathname.new(__dir__).parent if Rails.application
+
 ActiveRecord::Base.establish_connection(
   adapter: "sqlite3",
   database: ":memory:"
